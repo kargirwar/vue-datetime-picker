04b3fb6a3cd66ac5ec126c075a4a79ee10303e72
Pankaj Kargirwar <kargirwar@gmail.com>
2018-01-10 15:43:34 +0530

    Introduced calendar

--- src/Calendar.vue
+++ src/Calendar.vue
@@ -1,25 +1,20 @@
 <template lang="pug">
-    table.table
-        tr
-            td(v-for="(d, i) in weeks[0]")
-                span(v-bind:class="{active: d['active']}") {{d['day']}}
-        tr
-            td(v-for="(d, i) in weeks[1]")
-                span(v-bind:class="{active: d['active']}") {{d['day']}}
+    .calendar-container
+        .row
+            .col-sm-2(@click="prevMonth") Prev
+            .col-sm-8.text-center {{month}}
+            .col-sm-2(@click="nextMonth") Next
+        .row
+            .col-sm-12
+                table.table
+                    thead
+                        tr
+                            th(v-for="day in days") {{day}}
+                    tbody
+                        tr(v-for="week in weeks")
+                            td(v-for="d in week")
+                                span(v-bind:class="{today: d['today'], 'other-month': d['other-month']}") {{d['day']}}
 
-        tr
-            td(v-for="(d, i) in weeks[2]")
-                span(v-bind:class="{active: d['active']}") {{d['day']}}
-        tr
-            td(v-for="(d, i) in weeks[3]")
-                span(v-bind:class="{active: d['active']}") {{d['day']}}
-
-        tr
-            td(v-for="(d, i) in weeks[4]")
-                span(v-bind:class="{active: d['active']}") {{d['day']}}
-        tr
-            td(v-for="(d, i) in weeks[5]")
-                span(v-bind:class="{active: d['active']}") {{d['day']}}
 </template>
 
 <script>
@@ -32,41 +27,72 @@ export default {
     data: function() {
         return {
             weeks: [],
-            isActive: false
+            isActive: false,
+            month: '',
+            days: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
         }
     },
     created() {
-        var m = Moment().startOf('month').startOf('week').clone();
-        m.subtract(1, 'days');
-        var thisDay = Moment().format('D');
-        var thisMonth = Moment().format('M');
-        var isActive = false;
-        //we want a 7x7 grid starting from the sunday of the week for the 1st of month
-        for (var i = 0; i < ROWS; i++) {
-            var week = []
-            for (var j = 0; j < COLUMNS; j++) {
-                m.add(1, 'days');
-                if (thisDay == m.format('D') && thisMonth == m.format('M')) {
-                    isActive = true;
-                } else {
-                    isActive = false;
-                }
-                week.push({
-                    'day': m.format('D'),
-                    'active': isActive
-                });
-                console.log(m.format('D'));
+        var m = Moment().month(this.month);
+        this.fillCalendar(m.format('MMMM'));
+    },
+    methods: {
+        fillCalendar: function(month) {
+            var m = Moment().clone();
+            var currMonth = month;
+
+            if (month) {
+                m = m.month(month).clone();
             }
 
-            this.weeks.push(week);
-        }
+            this.month = m.format('MMM');
+            this.weeks = [];
+            m = m.startOf('month').startOf('week').clone();
+
+            var thisDay = Moment().format('D');
+            var thisMonth = Moment().format('M');
+            var isToday = false;
+
+            var isOtherMonth = false;
+
+            //we want a 7x7 grid starting from the sunday of the week for the 1st of month
+            for (var i = 0; i < ROWS; i++) {
+                var week = []
+                for (var j = 0; j < COLUMNS; j++) {
+                    isToday = (thisDay == m.format('D') && thisMonth == m.format('M')) ? true : false;
+                    isOtherMonth = (currMonth != m.format('MMMM')) ? true : false;
+
+                    week.push({
+                        'day': m.format('D'),
+                        'today': isToday,
+                        'other-month': isOtherMonth
+                    });
+                    m.add(1, 'days');
+                }
+
+                this.weeks.push(week);
+            }
+        },
+        nextMonth: function() {
+            var m = Moment().month(this.month);
+            m.add(1, 'month');
+            this.fillCalendar(m.format('MMMM'));
+        },
+        prevMonth: function() {
+            var m = Moment().month(this.month);
+            m.subtract(1, 'month');
+            this.fillCalendar(m.format('MMMM'));
+        },
     }
 }
 </script>
 
 <style scoped>
-.active {
+.today {
     color: #f00;
-    display:block;
+}
+
+.other-month {
+    color: #ccc;
 }
 </style>
